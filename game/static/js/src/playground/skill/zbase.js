2c3e4f4400e80f0a6aaed5f55bcca0942a086382
shiwuliua <3403456815@qq.com>
2023-01-25 19:56:13 +0800

    test2

---
+++ game/static/js/src/playground/skill/zbase.js
@@ -0,0 +1,44 @@
+class FireBall extends AcGameObject
+{
+	constructor(playground,player,x,y,radius,vx,vy,color,speed,move_length)
+	{
+		super();
+		this.playground=playground;
+		this.player=player;
+		this.ctx=this.playground.game_map.ctx;
+		this.x=x;
+		this.y=y;
+		this.vx=vx;
+		this.vy=vy;
+		this.radius=radius;
+		this.color=color;
+		this.speed=speed;
+		this.move_length=move_length;
+		this.eps=0.1;
+	}
+	start()
+	{
+
+	}
+	update()
+	{
+		if(this.move_lengh<this.eps)
+		{
+			this.destroy();
+			return false;
+		}
+		let moved=Math.min(this.move_length,this.speed * this.timedelta /1000);
+		this.x+=this.vx*moved;
+		this.y+=this.vy*moved;
+
+		this.render();
+
+	}
+	render()
+	{
+		this.ctx.beginPath();
+		this.ctx.arc(this.x,this.y,this.radius,0,Math.PI *2,false);
+		this.ctx.fillStyle=this.color;
+		this.ctx.fill();
+	}
+}
